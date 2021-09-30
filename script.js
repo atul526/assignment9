71b9a2b32a6c26c592948f83e944da8f6bf171c3
Atul chaudhary <91272081+atul526@users.noreply.github.com>
2021-09-30 16:10:17 +0530

    update

---
+++ script.js
@@ -0,0 +1,9 @@
+function getcube(){
+var number = document.getElementById("number").value;
+let cube = number*number*number;
+alert(number**3)
+document.writeln("The cube is = ", +(number**3),"</br>");
+alert(number**2)
+document.writeln("The square is = ", +(number**2),"</br>");
+
+}
